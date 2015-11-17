740fdfad30020c6142078e82646e5f9453a5e3fe
Duncan McGreggor <oubiwann@gmail.com>
2015-11-16 22:00:22 -0600

    Changed to depend upon the official 2.0.0 release.

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [nio "1.0.3"]
-                 [jmorton/gdal "2.0.1-SNAPSHOT"]])
+                 [org.gdal/gdal "2.0.0"]])
