3c55a33b73e4aaee6749e5f0f503f520fe3187c8
Duncan McGreggor <oubiwann@gmail.com>
2015-11-17 00:19:17 -0600

    Start REPL in core.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.gdal/gdal "2.0.0"]
-                 [nio "1.0.3"]])
+                 [nio "1.0.3"]]
+  :repl-options {:init-ns clj-gdal.core})
