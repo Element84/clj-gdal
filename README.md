812a03b1567ba789ef6b8694c2e13a84cd93fddb
Duncan McGreggor <oubiwann@gmail.com>
2015-12-21 12:19:03 -0600

    Updated versions, etc.

--- README.md
+++ README.md
@@ -55,7 +55,7 @@ clj-gdal.core=> (open "LC80280302015112LGN00_B1.TIF")
 
 ## License [&#x219F;](#contents)
 
-Copyright © 2015 Jonathan Morton
+Copyright © 2015 Jonathan Morton, Duncan McGreggor
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-gdal "0.1.0-SNAPSHOT"
+(defproject clj-gdal "0.1.0"
   :description "GDAL for Clojure"
-  :url "http://github.com/jmorton/clj-gdal"
+  :url "http://github.com/oubiwann/clj-gdal"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
