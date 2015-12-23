f83126f0b743ea1b28e39b8d61ee1ae2fff5d457
Duncan McGreggor <oubiwann@gmail.com>
2015-12-23 13:51:23 -0600

    Updated session code.

--- examples/repl-session.clj
+++ examples/repl-session.clj
@@ -2,10 +2,10 @@
 (def data-dir "/home/oubiwann/Documents/Landsat/Downloads")
 (def scene-id "LC80290302015263LGN00")
 (def tiff-file (format "%s/%s/%s_B2.TIF" data-dir scene-id scene-id))
-(def data (gdal/open tiff-file))
+(def tiff-data (gdal/open tiff-file))
 
-(dataset/get-size data)
-(dataset/count-bands data)
+(dataset/get-size tiff-data)
+(dataset/count-bands tiff-data)
 
-(def band-data (dataset/get-band data 1))
+(def band-data (dataset/get-band tiff-data 1))
 (band/get-checksum band-data)
