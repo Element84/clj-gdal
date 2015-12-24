7515f408cc34cf067040c5f2dae3c2f327afd64f
Duncan McGreggor <oubiwann@gmail.com>
2015-12-23 19:02:10 -0600

    Added new dep.

--- project.clj
+++ project.clj
@@ -3,9 +3,11 @@
   :url "http://github.com/oubiwann/clj-gdal"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["geotoolkit" "http://maven.geotoolkit.org/"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [org.gdal/gdal "1.11.2"]
+                 [org.apache.sis.core/sis-referencing "0.6"]
                  [nio "1.0.3"]]
   :repl-options {:init-ns gdal.dev})
