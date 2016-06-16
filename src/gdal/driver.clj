9e31fb7d8cec8d6f4983ccb0333b589f11910a61
Jonathan Morton <jon.morton@gmail.com>
2016-06-16 10:25:03 -0500

    Implement driver related functions

--- src/gdal/driver.clj
+++ src/gdal/driver.clj
@@ -2,53 +2,54 @@
   (:require [gdal.core])
   (:import [org.gdal.gdal Driver]))
 
-(defn register
-  "Register a driver for use"
-  [driver]
-  nil)
-
-(defn deregister
-  "Deregister the driver"
-  [driver]
-  nil)
-
-(defn get-help-topic
-  "URL to the help describing this driver"
-  [driver]
-  nil)
-
-(defn get-long-name
-  "Full name of a driver"
-  [driver]
-  nil)
-
-(defn get-short-name
-  "Succint name of a driver"
-  [driver]
-  nil)
-
-;; dataset manipulation
+(defn copy-files
+  "Not explained in GDAL Java bindings"
+  [driver new-name old-name]
+  ;; XXX understand and explain intended use.
+  (.CopyFiles driver new-name old-name))
 
 (defn create
   "Create a new dataset"
   [driver name xsize ysize]
+  ;; XXX support overloaded functions using options?
   (.Create driver name xsize ysize))
 
 (defn create-copy
   "Create a copy of a dataset"
-  [name dest]
-  nil)
+  [driver name src-dataset]
+  (.CreateCopy driver name src-dataset))
+
+(defn deregister
+  "Deregister the driver"
+  [driver]
+  (.Deregister driver))
+
+(defn get-help-topic
+  "URL to the help describing this driver"
+  [driver]
+  (.getHelpTopic driver))
+
+(defn get-long-name
+  "Full name of a driver"
+  [driver]
+  (.getLongName driver))
+
+(defn get-short-name
+  "Succint name of a driver"
+  [driver]
+  (.getShortName driver))
 
 (defn delete
   "Delete named dataset"
-  [name]
-  nil)
+  [driver dataset-name]
+  (.Delete driver dataset-name))
+
+(defn register
+  "Register a driver for use"
+  [driver]
+  (.Register driver))
 
 (defn rename
   "Change the name of a dataset"
-  [new-name old-name]
-  nil)
-
-(defn copy-files
-  ""
-  [new-name old-name])
+  [driver new-name old-name]
+  (.Rename driver new-name old-name))
--- test/gdal/driver_test.clj
+++ test/gdal/driver_test.clj
@@ -8,3 +8,11 @@
     (let [mem (gdal/get-driver-by-name "MEM")
           dataset (driver/create mem "test" 128 128)]
       (is (some? dataset)))))
+
+(deftest register-test
+  (testing "register a driver"
+    (comment "I don't know how to test this..."))
+
+(deftest deregister-test
+  (testing "deregister a driver"
+    (comment "I don't know how to test this...")))
